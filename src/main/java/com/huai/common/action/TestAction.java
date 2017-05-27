7c61cbdcf5f9d5d0c6b66af8773d7c32bf2d6409
huai45 <huai23@qq.com>
2017-05-27 22:48:51 +0800

    submit

--- src/main/java/com/huai/common/action/TestAction.java
+++ src/main/java/com/huai/common/action/TestAction.java
@@ -1,11 +1,14 @@
 package com.huai.common.action;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,6 +20,10 @@ import org.springframework.web.servlet.ModelAndView;
 public class TestAction {
 
 	private static final Logger log = Logger.getLogger(TestAction.class);
+
+	@Resource(name="jdbcTemplate2")
+	public JdbcTemplate jdbcTemplate2;
+
 	
 	@RequestMapping(value = "/testindexpage")   
     public ModelAndView testindexpage(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap)  {
@@ -28,7 +35,9 @@ public class TestAction {
 	@ResponseBody
     public Object testjson(HttpServletRequest request, HttpServletResponse response,
     		ModelMap modelMap)  {
-		log.info(" testjson  ");
+		log.info(" testjson  jdbcTemplate2 = "+jdbcTemplate2);
+		List tables = jdbcTemplate2.queryForList(" SELECT name FROM sqlite_master WHERE type='table' ");
+		log.info(" testjson  tables = "+tables.size());
         Map map = new HashMap();
         map.put("succes", true);
         map.put("msg", "hello");
